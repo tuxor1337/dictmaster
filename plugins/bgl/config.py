5b2c714dd85787042bc5216e5ca36b2a24a4dafd
tuxor1337 <tuxor1337@web.de>
2015-02-12 01:47:08 +0100

    Better bgl support.

--- plugins/bgl/config.py
+++ plugins/bgl/config.py
@@ -3,7 +3,19 @@
 import os
 
 cfg = {
-    "format" :  { "bgl" : "" }
+    "format" :  {
+        "bgl" : {
+            "definition": {
+                "regex": [
+                    [r"(?i)^[^<]+<br>\n<div[^>]+></div>", ""]
+                ],
+                "no_font_tags": ""
+            }
+        }
+    },
+    "editor": {
+        "dups": "enumerate"
+    }
 }
 
 cfg["name"] = os.path.split(os.path.dirname(os.path.abspath(__file__)))[1]
