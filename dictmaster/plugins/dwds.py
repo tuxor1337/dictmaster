83ccd9aa2320eab3d2d3902852479aa760b2a3d6
tuxor1337 <tuxor1337@web.de>
2015-03-03 00:25:21 +0100

    dwds: add support for etymological dictionary.

--- dictmaster/plugins/dwds.py
+++ dictmaster/plugins/dwds.py
@@ -13,16 +13,31 @@ from dictmaster.fetcher import Fetcher
 from dictmaster.postprocessor import HtmlContainerProcessor
 from dictmaster.editor import Editor
 
+PANEL_IDS = ["5","32"]
+DICTNAMES = {
+    "5": u"Etymologisches Wörterbuch © Dr. Wolfgang Pfeifer",
+    "32": u"Digitales Wörterbuch der deutschen Sprache",
+}
+
 class Plugin(PluginThread):
+    panelid = None
+
     def __init__(self, popts, dirname):
-        self.word_file = popts
+        if len(popts) != 2:
+            sys.exit("Error: Expected exactly two plugin params: "
+                +"a word list file and a panel id.")
+        self.word_file, self.panelid  = popts
         if not os.path.exists(self.word_file):
             sys.exit("Provide full path to (existing) word list file!")
-        super(Plugin, self).__init__(popts, dirname)
-        self.dictname = u"Digitales Wörterbuch der deutschen Sprache"
+        if self.panelid not in PANEL_IDS:
+            sys.exit("Panel ID {} is not supported. Try one of {}.".format(
+                self.panelid, ", ".join(PANEL_IDS)
+            ))
+        super(Plugin, self).__init__(popts, os.path.join(dirname, self.panelid))
+        self.dictname = DICTNAMES[self.panelid]
         self._stages = [
-            DWDSFetcher(self, threadcnt=10),
-            DWDSProcessor("", self, singleton=True),
+            DWDSFetcher(self),
+            DWDSProcessor(self),
             Editor(plugin=self)
         ]
 
@@ -33,7 +48,7 @@ class DWDSFetcher(Fetcher):
     class FetcherThread(Fetcher.FetcherThread):
         def filter_data(self, data):
             if data == None or len(data) < 2 \
-            or '<div id="ddc_panel_32"' not in data \
+            or '<div id="ddc_panel_' not in data \
             or '<p style="text-align: center;">Kein Eintrag vorhanden</p>' in data:
                 return None
             data = " ".join(data.split())
@@ -47,19 +62,42 @@ class DWDSFetcher(Fetcher):
             for r in regex: data = re.sub(r[0], r[1], data)
             parser = etree.HTMLParser(encoding="utf-8")
             doc = pq(etree.fromstring(data, parser=parser))
-            return doc("div#ddc_panel_32").html()
+            return doc("div.content_panel > div").html()
 
-        def parse_uri(self, uri):
-            return "http://m.dwds.de/loadpanel/?panel_id=32&qu=%s"%uri
+    def __init__(self, plugin):
+        super(DWDSFetcher, self).__init__(plugin, threadcnt=10)
+        self.FetcherThread.parse_uri = lambda fthread, uri: \
+            "http://m.dwds.de/loadpanel/?panel_id=%s&qu=%s"%(plugin.panelid,uri)
 
 class DWDSProcessor(HtmlContainerProcessor):
+    def __init__(self, plugin):
+        super(DWDSProcessor, self).__init__("", plugin, singleton=True)
+        self.do_html_definition = getattr(self, "do_html_definition_%s"%plugin.panelid)
+        self.do_html_alts = getattr(self, "do_html_alts_%s"%plugin.panelid)
+
     def do_html_term(self, doc):
         term = doc("span.wb_lzga").eq(0).text().strip()
         regex = []
         for r in regex: term = re.sub(r[0], r[1], term)
         return term
 
-    def do_html_definition(self, html, term):
+    def do_html_alts_5(self, html, term):
+        doc = pq(html)
+        return [doc(a).text() for a in doc("span.wb_lzga_min a")]
+
+    def do_html_alts_32(self, html, term): return []
+
+    def do_html_definition_5(self, html, term):
+        doc = pq(html)("div.wb_container_zone_s")
+        for a in doc("a"):
+            if doc(a).text().strip() == "": doc(a).replaceWith(doc(a).text())
+            else:
+                href = "bword://%s" % doc(a).text().strip(". ").lower()
+                doc(a).attr("href", href)
+        doc("*").removeAttr("class").removeAttr("id")
+        return " ".join(doc.html().strip().split())
+
+    def do_html_definition_32(self, html, term):
         doc = pq(html)
         doc("img,audio,script").remove()
         doc("div.wb_zone_v").remove()
@@ -91,8 +129,6 @@ class DWDSProcessor(HtmlContainerProcessor):
             doc(a).replaceWith("" if replacement == None else replacement)
         for a in doc("a"):
             if doc(a).text().strip() == "": doc(a).replaceWith(doc(a).text())
-            elif "definition/english/" not in doc(a).attr("href"):
-                doc(a).replaceWith(doc(a).html())
             else:
                 href = "bword://%s" % doc(a).text().strip(". ").lower()
                 doc(a).attr("href", href)
